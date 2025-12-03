ea5558207282de70c37d7d722a917a8ff6c45078
3omda-Orchida <mohamedemadatorchida@gmail.com>
2025-12-03 16:38:15 +0200

    Documentation file has been added

--- src/WebAPI/obj/Debug/net8.0/OnionTemplate.WebAPI.AssemblyInfo.cs
+++ src/WebAPI/obj/Debug/net8.0/OnionTemplate.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnionTemplate.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01dfd4b9c279d090201be26bcc57094a657f0dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ca33a6fb9130d6e023e2a08f21dc4c036fb974")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnionTemplate.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnionTemplate.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
